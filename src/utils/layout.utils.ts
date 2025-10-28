d4615d12e401cc79e6f0a08c728dbd4acbd1b338
Domen Gorjup <domen.gorjup@specto.si>
2025-10-28 08:50:17 +0100

    fix: remove lang and theme for now

--- src/utils/layout.utils.ts
+++
@@ -1,7 +0,0 @@
-export const changeTheme = (): void => {
-  const theme = document.documentElement.getAttribute('data-theme');
-  document.documentElement.setAttribute(
-    'data-theme',
-    theme === 'light' ? 'dark' : 'light'
-  );
-};
